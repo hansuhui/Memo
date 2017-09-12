3fd5f0b3f75b1bce27f6fd2f36ad7ce2caa35e62
HanSuHui <poixgks@naver.com>
2017-09-12 14:11:02 +0900

    NOde

---
+++ NodeJS/NodeJS 기초/Js/Mongose/app.js
@@ -0,0 +1,36 @@
+﻿// [LOAD PACKAGES]
+
+var express 	=  require("express");
+var app 	=  express();
+var bodyParser 	=  require("body-parser");
+var mongoose	=  require("mongoose");
+mongoose.Promise = global.Promise ;
+
+
+// [CONFIGURE APP TO USE bodyParser]
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+
+// [CONFIGURE SERVER PORT]
+var port = process.env.PORT || 8080;
+
+
+// [RUN SERVER]
+var server = app.listen(port,function(){
+   console.log("Express server has started on port "+ port);
+});
+
+
+// [CONFIGURE mongoose]
+
+// CONNECT TO MONGODB SERVER
+var db = mongoose. connection;
+db.on("error", console.error);
+mongoose.connect("mongodb://localhost/poixgks",{useMongoClient: true});
+
+// DEFINE MODEL
+var Book = require("./models/book");
+
+// [CONFIGURE ROUTER]
+var router = require("./routes")(app,Book);
